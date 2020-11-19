a3265d95050543157740258fa8492e0aa7c9d653
AndreyKarahojaev <45915552+AndreyKarahojaev@users.noreply.github.com>
2020-11-19 18:19:40 +0400

    Обновление

--- js/script.js
+++ js/script.js
@@ -28,10 +28,10 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
+        dots[i].className = dots[i].className.replace("current", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
+    dots[slideIndex - 1].className += "current";
 }
 
 // Слайдер сервис
